1da4d2e30b7d7c163b6da629c59b72f901cee7f8
Mozillazg <opensource.mozillazg@gmail.com>
2014-03-30 20:11:15 +0800

    Split api feature to API class

--- shanbay/__init__.py
+++ shanbay/__init__.py
@@ -29,6 +29,20 @@ class APIServerException(ShanbayException):
     pass
 
 
+def _api_wrapper(func):
+    @wraps(func)
+    def _wrapper(*args, **kwargs):
+        url, method, shanbay = func(*args, **kwargs)
+        r = shanbay._request(url, method, **kwargs)
+        if r.url.startswith('http://www.shanbay.com/accounts/login/'):
+            raise AuthException
+        try:
+            return r.json()
+        except:
+            raise APIServerException
+    return _wrapper
+
+
 class Shanbay(object):
     USER_AGENT = 'python-shanbay/%s' % __version__
 
@@ -62,51 +76,47 @@ class Shanbay(object):
             'next': '',
         }
         r = self._request(url, 'post', data=data)
-        if r.url == 'http://www.shanbay.com/accounts/login/':
+        if r.url.startswith('http://www.shanbay.com/accounts/login/'):
             raise AuthException
 
-    def _api_wrapper(func):
-        @wraps(func)
-        def _wrapper(*args, **kwargs):
-            url, method, self = func(*args, **kwargs)
-            r = self._request(url, method, allow_redirects=False, **kwargs)
-            if r.url == 'http://www.shanbay.com/accounts/login/':
-                raise AuthException
-            try:
-                return r.json()
-            except:
-                raise APIServerException
-        return _wrapper
+    @property
+    def api(self):
+        return API(self)
+
+
+class API(object):
+    def __init__(self, shanbay):
+        self.shanbay = shanbay
 
     @_api_wrapper
     def user_info(self, **kwargs):
         url = 'http://www.shanbay.com/api/user/info/'
-        return url, 'get', self
+        return url, 'get', self.shanbay
 
     @_api_wrapper
     def query_word(self, word, **kwargs):
         url = 'http://www.shanbay.com/api/word/%s' % quote(word)
-        return url, 'get', self
+        return url, 'get', self.shanbay
 
     @_api_wrapper
     def add_word(self, word, **kwargs):
         url = 'http://www.shanbay.com/api/learning/add/%s' % word
-        return url, 'get', self
+        return url, 'get', self.shanbay
 
     @_api_wrapper
     def examples(self, learn_id, **kwargs):
         url = 'http://www.shanbay.com/api/learning/examples/%s'
         url = url % learn_id
-        return url, 'get', self._request
+        return url, 'get', self.shanbay
 
     @_api_wrapper
     def add_example(self, learn_id, question, answer, **kwargs):
         url = 'http://www.shanbay.com/api/example/add/%s?sentence=%s&translation=%s'
         url = url % (learn_id, question, answer)
-        return url, 'get', self
+        return url, 'get', self.shanbay
 
     @_api_wrapper
     def add_note(self, learn_id, note, **kwargs):
         url = 'http://www.shanbay.com/api/note/add/%s?note=%s'
         url = url % (learn_id, quote(note))
-        return url, 'get', self
+        return url, 'get', self.shanbay
